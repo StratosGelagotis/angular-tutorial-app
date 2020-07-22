7d141df4df5759240cd5145250148177b02d7605
Stratos Gelagotis <Stratos.Gelagotis@eurodyn.com>
2020-07-22 12:59:25 +0300

    Added checkout Form and Pricing Details

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,10 +9,27 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items;
-  constructor(private cartService: CartService) { }
+  checkoutForm;
+
+  constructor(private cartService: CartService,
+    private formBuilder: FormBuilder) {
+      this.checkoutForm = this.formBuilder.group({
+        name:'',
+        address:'',
+        email:'',
+        phone:''
+      });
+     }
 
   ngOnInit(): void {
     this.items = this.cartService.getItems();
   }
 
+  onSubmit(customerData) {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.info('Your order has been submitted', customerData);
+  }
 }
